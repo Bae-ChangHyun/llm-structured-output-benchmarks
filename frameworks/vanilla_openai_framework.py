dfd9969e509edee28d43c908f77e2e94323b20d3
chbae <chbae@gcsc.co.kr>
2025-04-23 13:19:07 +0900

    🔊 add log

--- frameworks/vanilla_openai_framework.py
+++ frameworks/vanilla_openai_framework.py
@@ -1,6 +1,8 @@
 from typing import Any
+import os
 
 from openai import OpenAI
+from loguru import logger
 
 from frameworks.base import BaseFramework, experiment
 
@@ -8,7 +10,15 @@ from frameworks.base import BaseFramework, experiment
 class VanillaOpenAIFramework(BaseFramework):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        
+        api_key = os.environ.get("OPENAI_API_KEY")
+        if not api_key:
+            logger.error("OPENAI_API_KEY is not set in the environment variables!")
+            raise ValueError("Export OEPN_API_KEY in your environment variables.")
+            
         self.openai_client = OpenAI()
+        
+        logger.info("OpenAI 클라이언트가 초기화되었습니다.")
 
     def run(
         self, task: str, n_runs: int, expected_response: Any = None, inputs: dict = {}
@@ -25,4 +35,5 @@ class VanillaOpenAIFramework(BaseFramework):
             return response.choices[0].message.parsed
 
         predictions, percent_successful, metrics, latencies = run_experiment(inputs)
+        logger.info(f"실행 결과 - 성공률: {percent_successful}, 응답 수: {len(predictions) if predictions else 0}")
         return predictions, percent_successful, metrics, latencies
