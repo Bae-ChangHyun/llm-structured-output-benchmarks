00c85f7f042f52553bdaaccbfa19aa2d1e85eb35
chbae <chbae@gcsc.co.kr>
2025-05-16 17:44:11 +0900

    Fix: support vanilla_ollama(before openai)

--- frameworks/vanilla_ollama_framework.py
+++ frameworks/vanilla_ollama_framework.py
@@ -1,7 +1,8 @@
 import os
-from typing import Any
+from typing import Any, Dict
+import json
 
-from openai import OpenAI, base_url
+from ollama import chat
 from loguru import logger
 
 from frameworks.base import BaseFramework, experiment
@@ -13,25 +14,25 @@ class VanillaOllamaFramework(BaseFramework):
     """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        base_url = os.environ.get("OLLAMA_HOST") if os.environ.get("OLLAMA_HOST") else "http://localhost:11434/v1"
-        self.openai_client = OpenAI(base_url=os.environ['OLLAMA_HOST'], api_key="1")
         
-        logger.info("Ollama 클라이언트가 초기화되었습니다.")
-
     def run(
         self, n_runs: int, expected_response: Any = None, inputs: dict = {}
     ) -> tuple[list[Any], float, dict, list[list[float]]]:
         @experiment(n_runs=n_runs, expected_response=expected_response)
         def run_experiment(inputs):
-            response = self.openai_client.beta.chat.completions.parse(
+            response = chat(
                 model=self.llm_model,
-                response_format=self.response_model,
+                format=self.response_model.model_json_schema(),
                 messages=[
                     {"role": "user", "content": self.prompt.format(**inputs)}
                 ],
+                options={'temperature': 0},
             )
-            return response.choices[0].message.parsed
+            content = json.loads(response.message.content)
+            
+            return content
 
         predictions, percent_successful, metrics, latencies = run_experiment(inputs)
+        
         logger.info(f"실행 결과 - 성공률: {percent_successful}, 응답 수: {len(predictions) if predictions else 0}")
         return predictions, percent_successful, metrics, latencies
