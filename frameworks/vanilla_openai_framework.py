c77302a2bf15c7b9f38811e0679f240dd32d377d
chbae <chbae@gcsc.co.kr>
2025-05-16 17:43:10 +0900

    Feat: supprot ollama models on vanilla openai_framework

--- frameworks/vanilla_openai_framework.py
+++ frameworks/vanilla_openai_framework.py
@@ -11,14 +11,21 @@ class VanillaOpenAIFramework(BaseFramework):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         
-        api_key = os.environ.get("OPENAI_API_KEY")
-        if not api_key:
-            logger.error("OPENAI_API_KEY is not set in the environment variables!")
-            raise ValueError("Export OEPN_API_KEY in your environment variables.")
-            
-        self.openai_client = OpenAI()
+        if self.llm_model_host == "openai":
+            api_key = os.environ.get("OPENAI_API_KEY")
+            if not api_key:
+                logger.error("OPENAI_API_KEY is not set in the environment variables!")
+                raise ValueError("Export OEPN_API_KEY in your environment variables.")
+                
+            self.openai_client = OpenAI()
         
-        logger.info("OpenAI 클라이언트가 초기화되었습니다.")
+            logger.info("OpenAI 클라이언트가 초기화되었습니다.")
+        elif self.llm_model_host == "ollama":
+            self.openai_client = OpenAI(
+                base_url=os.environ['OLLAMA_HOST'],
+                api_key="ollama",
+            )
+            logger.info("Ollama 클라이언트가 초기화되었습니다.")
 
     def run(
         self, n_runs: int, expected_response: Any = None, inputs: dict = {}
@@ -32,6 +39,7 @@ class VanillaOpenAIFramework(BaseFramework):
                     {"role": "user", "content": self.prompt.format(**inputs)}
                 ],
             )
+            logger.debug(f"예측 결과: {response.choices[0].message.parsed}")
             return response.choices[0].message.parsed
 
         predictions, percent_successful, metrics, latencies = run_experiment(inputs)
