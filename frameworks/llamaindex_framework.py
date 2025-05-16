3ab7afa447d1abdaeb6073b69d622c1748f7114f
chbae <chbae@gcsc.co.kr>
2025-05-16 17:43:27 +0900

    Feat: supprot ollama models on llamaindex_framework

--- frameworks/llamaindex_framework.py
+++ frameworks/llamaindex_framework.py
@@ -1,4 +1,8 @@
+import os
+from pyexpat import model
 from typing import Any
+from openai import OpenAI
+from loguru import logger
 
 from llama_index.program.openai import OpenAIPydanticProgram
 
@@ -8,12 +12,25 @@ from frameworks.base import BaseFramework, experiment
 class LlamaIndexFramework(BaseFramework):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
+        
+        if self.llm_model_host == "openai":
+            api_key = os.environ.get("OPENAI_API_KEY")
+            if not api_key:
+                logger.error("OPENAI_API_KEY is not set in the environment variables!")
+                raise ValueError("Export OEPN_API_KEY in your environment variables.")
+                
+            self.openai_client = OpenAI()
+        elif self.llm_model_host == "ollama":
+            self.openai_client = OpenAI(
+                base_url=os.environ['OLLAMA_HOST'],
+                api_key="ollama",
+            )
+            
         # TODO: Swap the Program based on self.llm_model
         self.llamaindex_client = OpenAIPydanticProgram.from_defaults(
             output_cls=self.response_model,
             prompt_template_str=self.prompt,
-            llm_model=self.llm_model,
+            llm_model=self.openai_client,
         )
 
     def run(
