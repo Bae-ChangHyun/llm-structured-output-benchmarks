25017574c4c259b5150de2ad11b6d35846342714
chbae <chbae@gcsc.co.kr>
2025-05-16 17:44:28 +0900

    Feat: supprot ollama,google models on instructor_framework

--- frameworks/instructor_framework.py
+++ frameworks/instructor_framework.py
@@ -1,27 +1,48 @@
+import os
 from typing import Any
 
 import instructor
 from openai import OpenAI
+import google.generativeai as genai
 
 from frameworks.base import BaseFramework, experiment
 
 
 class InstructorFramework(BaseFramework):
+    # https://python.useinstructor.com
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.instructor_client = instructor.patch(OpenAI())
+        if self.llm_model_host == "openai":
+            self.instructor_client = instructor.from_openai(OpenAI())
+        elif self.llm_model_host == "ollama":
+            self.instructor_client = instructor.from_openai(OpenAI(
+                base_url = os.environ['OLLAMA_HOST'],
+                api_key = "ollama",
+            ),mode=instructor.Mode.JSON,)
+        elif self.llm_model_host == "google":
+            self.instructor_client = instructor.from_gemini(
+                client=genai.GenerativeModel(model_name=self.llm_model),
+                mode=instructor.Mode.GEMINI_JSON,
+            )
 
     def run(
         self, n_runs: int, expected_response: Any = None, inputs: dict = {}
     ) -> tuple[list[Any], float, dict, list[list[float]]]:
         @experiment(n_runs=n_runs, expected_response=expected_response)
         def run_experiment(inputs):
-            response = self.instructor_client.chat.completions.create(
-                model=self.llm_model,
-                response_model=self.response_model,
-                max_retries=self.retries,
-                messages=[{"role": "user", "content": self.prompt.format(**inputs)}],
-            )
+            if self.llm_model_host == "google":
+                response = self.instructor_client.chat.completions.create(
+                    response_model=self.response_model,
+                    max_retries=self.retries,
+                    messages=[{"role": "user", "content": self.prompt.format(**inputs)}],
+                )
+            else:
+                response = self.instructor_client.chat.completions.create(
+                    model=self.llm_model,
+                    response_model=self.response_model,
+                    max_retries=self.retries,
+                    messages=[{"role": "user", "content": self.prompt.format(**inputs)}],
+                )
             return response
 
         predictions, percent_successful, metrics, latencies = run_experiment(inputs)
